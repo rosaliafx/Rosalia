a5bd379f12d003d7b4225fc37512c3331bb06331
Eugene Guryanov <guryanovev@gmail.com>
2013-01-26 15:03:21 +0400

    Changed: sample workflow content.

--- Src/Rosalia.Build/Assets/MainWorkflow.cs.pp
+++ Src/Rosalia.Build/Assets/MainWorkflow.cs.pp
@@ -13,13 +13,13 @@
             {
                 return Sequence(
                     //// Task 1
-                    Task((result, c) => c.Logger.Info("Hello...")),
+                    Task((result, c) => result.AddInfo("Hello...")),
 
                     //// Task 2
                     Task((result, c) => c.Data.Message = "...world"),
 
                     //// Task 3
-                    Task((result, c) => c.Logger.Info(c.Data.Message))
+                    Task((result, c) => result.AddInfo(c.Data.Message))
                 );
             }
         }
