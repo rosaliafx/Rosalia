7655e9d087ff90f5fecbcbf53565c97b38de6105
EugeneGuryanov <eguryanov@codereign.net>
2015-03-07 18:04:48 +0300

    migrated: compression task.

---
+++ Src/Rosalia.TaskLib.Compression/CompressTask.cs
@@ -0,0 +1,64 @@
+﻿namespace Rosalia.TaskLib.Compression
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using ICSharpCode.SharpZipLib.Zip;
+    using Rosalia.Core.Tasks;
+    using Rosalia.Core.Tasks.Results;
+    using Rosalia.FileSystem;
+
+    public class CompressTask : AbstractTask, IEnumerable
+    {
+        private readonly IList<FileToCompress> _sourceFiles = new List<FileToCompress>();
+
+        public CompressTask(IFile destination)
+        {
+            Destination = destination;
+        }
+
+        public IFile Destination { get; private set; }
+
+        public void Add(string entityPath, IFile file)
+        {
+            _sourceFiles.Add(new FileToCompress(file, entityPath));
+        }
+
+        protected override ITaskResult<Nothing> SafeExecute(TaskContext context)
+        {
+            if (Destination == null)
+            {
+                throw new Exception("Destination file is not set");
+            }
+
+            using (var zipStream = new ZipOutputStream(Destination.WriteStream))
+            {
+                foreach (var fileToCompress in _sourceFiles)
+                {
+                    var entry = new ZipEntry(fileToCompress.EntityPath)
+                    {
+                        Size = fileToCompress.File.Length
+                    };
+
+                    zipStream.PutNextEntry(entry);
+
+                    using (var fileReadStream = fileToCompress.File.ReadStream)
+                    {
+                        fileReadStream.CopyTo(zipStream);
+                    }
+
+                    zipStream.CloseEntry();
+                }
+
+                zipStream.IsStreamOwner = true;
+            }
+
+            return Success;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _sourceFiles.GetEnumerator();
+        }
+    }
+}
---
+++ Src/Rosalia.TaskLib.Compression/FileToCompress.cs
@@ -0,0 +1,17 @@
+﻿namespace Rosalia.TaskLib.Compression
+{
+    using Rosalia.FileSystem;
+
+    public class FileToCompress
+    {
+        public FileToCompress(IFile file, string entityPath)
+        {
+            File = file;
+            EntityPath = entityPath;
+        }
+
+        public IFile File { get; private set; }
+
+        public string EntityPath { get; private set; }
+    }
+}
