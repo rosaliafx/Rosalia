434394ade939b7d7d8b3b569dd3c41143a3ce4e6
EugeneGuryanov <eguryanov@codereign.net>
2015-03-19 09:07:23 +0300

    runner cleanups

--- Src/Rosalia.Runner.Console/CommandLine/Support/ProgramContext.cs
+++ Src/Rosalia.Runner.Console/CommandLine/Support/ProgramContext.cs
@@ -8,12 +8,19 @@
     public class ProgramContext
     {
         public string[] Args { get; set; }
+
         public OptionsConfig OptionsConfig { get; set; }
+
         public RosaliaOptions Options { get; set; }
+
         public ILogRenderer LogRenderer { get; set; }
+
         public LogHelper Log { get; set; }
+
         public IDirectory WorkDirectory { get; set; }
+
         public IFile InputFile { get; set; }
+
         public IWorkflow Workflow { get; set; }
     }
 }
