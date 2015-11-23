880e254c59bea104cc175f3551af9543ed004b6a
rosaliafx <guryanovev+rosalia@gmail.com>
2015-11-23 15:00:22 +0300

    added recover for tasks of Nothing

--- Src/Rosalia.Core/Api/Extensions_Recovery.cs
+++ Src/Rosalia.Core/Api/Extensions_Recovery.cs
@@ -2,9 +2,15 @@
 {
     using System;
     using Rosalia.Core.Tasks;
+    using Rosalia.Core.Tasks.Results;
 
     public static partial class Extensions
     {
+        public static ITask<Nothing> Recover(this ITask<Nothing> task) 
+        {
+            return new RecoverTask<Nothing>(task, Nothing.Value);
+        }
+
         public static ITask<TResult> RecoverWith<TResult>(this ITask<TResult> task, Func<TResult> valueProvider) where TResult : class
         {
             return new RecoverTask<TResult>(task, valueProvider);
