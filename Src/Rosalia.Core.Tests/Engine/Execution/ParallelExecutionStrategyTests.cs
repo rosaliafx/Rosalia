37ba879ab46f6a62b31f250556377271d6dff501
rosaliafx <guryanovev+rosalia@gmail.com>
2016-06-01 17:36:09 +0300

    Moving to the latest version of NUnit Added a flag to ignore framework version on generating NuSpec from packages.config

--- Src/Rosalia.Core.Tests/Engine/Execution/ParallelExecutionStrategyTests.cs
+++ Src/Rosalia.Core.Tests/Engine/Execution/ParallelExecutionStrategyTests.cs
@@ -1,7 +1,9 @@
 ﻿namespace Rosalia.Core.Tests.Engine.Execution
 {
+    using NUnit.Framework;
     using Rosalia.Core.Engine.Execution;
 
+    [TestFixture]
     public class ParallelExecutionStrategyTests : ExecutionStrategyTests
     {
         protected override IExecutionStrategy CreateStrategy()
